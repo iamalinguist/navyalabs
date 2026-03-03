1a9884265054a02ebb0b521b725faf764fc6bfff
Vivek Tripathi <sopan.tripathi@gmail.com>
2026-03-03 18:41:30 +0000

    i am to do it by cmd in my local compter.you can give me steps

--- next.config.js
+++ next.config.js
@@ -1,38 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'iamalinguist.github.io',
-        port: '',
-        pathname: '/**',
-      },
-    ],
   },
 };
 
